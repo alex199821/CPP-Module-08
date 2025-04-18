e788a59dc8e53c69b6381127e6bdd4a21e4296f9
macbook <macbook@macbooks-MacBook-Pro.local>
2025-04-18 02:25:43 +0200

    add tests ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: auplisas <auplisas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:18:33 by auplisas          #+#    #+#             */
-/*   Updated: 2025/04/17 17:25:50 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/04/18 02:24:12 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
-{
-    return (0);
+#include "easyfind.hpp"
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    try {
+        auto position = easyfind(numbers, 3);
+        std::cout << "Found number: " << *position << std::endl;
+    } catch (const std::exception& error) {
+        std::cerr << error.what() << std::endl;
+    }
+
+
+    try {
+        auto position = easyfind(numbers, 121);
+        std::cout << "Found number: " << *position << std::endl;
+    } catch (const std::exception& error) {
+        std::cerr << error.what() << std::endl;
+    }
+
+    return 0;
 }
