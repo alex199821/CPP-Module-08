cb8ce1d559c4c7e45479d3e710bf7211222b681e
macbook <macbook@macbooks-MacBook-Pro.local>
2025-04-18 02:22:27 +0200

    add: easyfind done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,10 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: auplisas <auplisas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:20:42 by auplisas          #+#    #+#             */
-/*   Updated: 2025/04/17 17:20:42 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/04/18 02:18:43 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+template <typename T> typename T::const_iterator easyfind(const T &intContainer,
+	int numberToFind)
+{
+	auto foundPosition = std::find(intContainer.begin(), intContainer.end(),
+			numberToFind);
+
+	if (foundPosition == intContainer.end())
+	{
+		throw std::runtime_error("Number not found in the container.");
+	}
+
+	return (foundPosition);
+}
