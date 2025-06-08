30be614d5c7c11fba980a1ad2113d607f93aec5c
macbook <macbook@macbooks-MacBook-Pro.local>
2025-06-08 19:43:35 +0200

    add: more test on main.cpp in ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 18:44:34 by macbook           #+#    #+#             */
-/*   Updated: 2025/04/21 03:30:21 by macbook          ###   ########.fr       */
+/*   Updated: 2025/06/08 19:37:10 by macbook          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,51 @@ int	main(void)
 		++it;
 	}
 	std::stack<int> s(mstack);
+	
+	std::cout << "\n=== Additional Tests ===" << std::endl;
+	
+	std::cout << "\n--- Copy Constructor Test ---" << std::endl;
+	MutantStack<int> mstack2(mstack);
+	std::cout << "Original size: " << mstack.size() << std::endl;
+	std::cout << "Copy size: " << mstack2.size() << std::endl;
+	
+	std::cout << "\n--- Assignment Operator Test ---" << std::endl;
+	MutantStack<int> mstack3;
+	mstack3.push(99);
+	mstack3 = mstack;
+	std::cout << "Assigned stack size: " << mstack3.size() << std::endl;
+	
+	std::cout << "\n--- Empty Stack Test ---" << std::endl;
+	MutantStack<int> empty_stack;
+	std::cout << "Empty stack size: " << empty_stack.size() << std::endl;
+	std::cout << "Empty begin == end: " << (empty_stack.begin() == empty_stack.end()) << std::endl;
+	
+	std::cout << "\n--- String Stack Test ---" << std::endl;
+	MutantStack<std::string> str_stack;
+	str_stack.push("Hello");
+	str_stack.push("World");
+	str_stack.push("Test");
+	
+	for (MutantStack<std::string>::iterator str_it = str_stack.begin(); str_it != str_stack.end(); ++str_it)
+	{
+		std::cout << *str_it << " ";
+	}
+	std::cout << std::endl;
+	
+	std::cout << "\n--- Reverse Iteration Test ---" << std::endl;
+	MutantStack<int> rev_stack;
+	for (int i = 1; i <= 5; ++i)
+		rev_stack.push(i);
+	
+	MutantStack<int>::iterator rev_it = rev_stack.end();
+	--rev_it;
+	std::cout << "Reverse order: ";
+	while (rev_it != rev_stack.begin())
+	{
+		std::cout << *rev_it << " ";
+		--rev_it;
+	}
+	std::cout << *rev_it << std::endl;
+	
 	return (0);
 }
